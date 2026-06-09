f56870e697cb37cee03ec52bd208e1a8d3333ced
GuilhermeKK23 <gui.kavakami@gmail.com>
2026-06-09 00:20:44 -0300

    Add hash_file.sh and test.sh

---
+++ Codigo/Extra/hash_file.sh
@@ -0,0 +1,8 @@
+# Para usar, cria também o hash.sh e roda:
+# bash hash_file.sh A.cpp
+# as linhas que só tem } geram hashs diferentes, mas o resto tá certo
+while IFS= read -r l; do
+	echo "$l" > line.txt
+    hl=$(bash hash.sh line.txt 1 1000 | cut -c1-3)
+	echo -e "$hl $l"
+done < "$1"
