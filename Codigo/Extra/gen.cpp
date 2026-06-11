26ded0adb9cc4b4032c4e9d0d471c38a81b54a84
GuilhermeKK23 <gui.kavakami@gmail.com>
2026-06-10 21:22:20 -0300

    Fix pdf

--- Codigo/Extra/gen.cpp
+++
@@ -1,3 +0,0 @@
-int main(int argc, char** argv){
-    mt19937 rng(atoi(argv[1]));
-}
--- Codigo/Extra/hash_file.sh
+++
@@ -1,5 +0,0 @@
-while IFS= read -r l; do
-	echo "$l" > line.txt
-    hl=$(bash hash.sh line.txt 1 1000 | cut -c1-3)
-	echo -e "$hl $l"
-done < "$1"
